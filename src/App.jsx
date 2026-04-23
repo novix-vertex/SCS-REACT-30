fc9e7771e581622e536ccdd5926bb72bef19e6a3
chirag jain <jchirag0610@gmail.com>
2026-04-24 03:14:01 +0530

    background color correction, others-section image correction

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Other from './components/Other'
 
 const App = () => {
   return (
-    <main className='p-8 h-screen'>
+    <main className='p-8 bg-[#f5f5f5]'>
       <Navbar />
       <Hero />
       <Explore />
