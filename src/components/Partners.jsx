f1b638093f07605199312efd85c5b066ae07330e
chirag jain <jchirag0610@gmail.com>
2026-04-24 02:11:48 +0530

    partners section added, logo image need to change

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Partners = () => {
   return (
-    <div>Partners</div>
+    <div className='flex justify-between items-center gap-10 mt-8 mb-8'>
+      <h5 className='w-60 text-gray-600 font-[500]'>Backed by the best companies and visionary angels.</h5>
+      <div className='partners-logo flex gap-2 justify-end items-center grow-1'>
+        <img className="h-10 aspect-auto" src="/lenovo.png" alt="logo" />
+        <img className="h-10 aspect-auto" src="/lenovo.png" alt="logo" />
+        <img className="h-10 aspect-auto" src="/lenovo.png" alt="logo" />
+        <img className="h-10 aspect-auto" src="/lenovo.png" alt="logo" />
+        <img className="h-10 aspect-auto" src="/lenovo.png" alt="logo" />
+        <img className="h-10 aspect-auto" src="/lenovo.png" alt="logo" />
+        <img className="h-10 aspect-auto" src="/lenovo.png" alt="logo" />
+      </div>
+    </div>
   )
 }
 
