573e0e8c8bfd046f4ced8dcc9f41ace890df5f7e
chirag jain <jchirag0610@gmail.com>
2026-04-24 01:35:08 +0530

    facts section added

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Explore = () => {
   return (
-    <div className='flex justify-between items-start'>
+    <div className='flex justify-between items-start pt-10 pb-10'>
       <div>
-        <h2 className='text-4xl font-[400]'>Where Money Grows</h2>
+        <h2 className='text-4xl font-[400]'>What is USD Bloom?</h2>
         <button className='bg-black rounded-4xl text-white px-8 py-1 mt-2'>Explore now</button>
       </div>
       <h4 className='w-120 text-xl font-[500] text-gray-600'>A programmable, utility-driven stable token designed for native value accrual and seamless integration into Defi.</h4>
--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Facts = () => {
   return (
-    <div>Facts</div>
+    <div className='flex justify-between item-center mt-10 mb-10 gap-10'>
+
+      <div className='card1 h-80 flex grow-1 flex-col justify-between items-start bg-[url("/background.png")] bg-bottom-right bg-cover p-5 rounded-xl'>
+        <h2 className='text-3xl font-[400]'>Capital that grows</h2>
+        <h4 className='w-75 text-xl font-[500] text-gray-600'>Earn passive income as your stablecoins are deployed into high-performing Defi protocols.</h4>
+      </div>
+
+      <div className='card2 h-80 flex flex-col justify-between items-start bg-[#2B2742] bg-bottom-right bg-cover p-5 rounded-xl'>
+        <h2 className='text-3xl text-white font-[400]'>Always liquid,<br />always stable</h2>
+        <h4 className='w-75 text-xl font-[500] text-slate-400'>Stayfully dollar-pegged with instant access to your funds - no lockups or delays.</h4>
+      </div>
+
+      <div className='card3 h-80 flex flex-col justify-between items-start bg-[#2B2742] bg-bottom-right bg-cover p-5 rounded-xl'>
+        <h2 className='text-3xl text-white font-[400]'>100%<br />hands-free</h2>
+        <h4 className='w-75 text-xl font-[500] text-slate-400'>No need to manage strategies manually. USD Bloom works in the background for you.</h4>
+      </div>
+
+
+    </div>
   )
 }
 
