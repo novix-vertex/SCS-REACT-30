515360fe4ab52255f8b438aa65d5296f5e54d78d
chirag jain <jchirag0610@gmail.com>
2026-04-24 03:06:42 +0530

    space added vertically on partners sectoin

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Partners = () => {
   return (
-    <div className='flex justify-between items-center gap-10 mt-8 mb-8'>
+    <div className='flex justify-between items-center gap-10 mt-15 mb-30'>
       <h5 className='w-60 text-gray-600 font-[500]'>Backed by the best companies and visionary angels.</h5>
       <div className='partners-logo flex gap-2 justify-end items-center grow-1'>
         <img className="h-10 aspect-auto" src="/lenovo.png" alt="logo" />
