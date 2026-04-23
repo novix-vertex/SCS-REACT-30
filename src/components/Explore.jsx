3a9fe3303e4854fc2080fbb85cf9e65e84d89827
chirag jain <jchirag0610@gmail.com>
2026-04-24 01:12:48 +0530

    explore section added

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Explore = () => {
   return (
-    <div>Explore</div>
+    <div className='flex justify-between items-start'>
+      <div>
+        <h2 className='text-4xl font-[400]'>Where Money Grows</h2>
+        <button className='bg-black rounded-4xl text-white px-8 py-1 mt-2'>Explore now</button>
+      </div>
+      <h4 className='w-120 text-xl font-[500] text-gray-600'>A programmable, utility-driven stable token designed for native value accrual and seamless integration into Defi.</h4>
+
+    </div>
   )
 }
 
