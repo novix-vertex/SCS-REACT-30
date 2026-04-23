64b13a17f17cfc893de59adea93a319146632a30
chirag jain <jchirag0610@gmail.com>
2026-04-24 03:40:34 +0530

    images links corrected to publish on github

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='flex flex-col justify-center items-center bg-[url("/hero.png")] h-170 mt-5 mb-5 bg-bottom rounded-2xl gap-5'>
-      <img className="h-10" src="/logo.webp" alt="" />
+      <img className="h-10" src="./logo.webp" alt="" />
       <h1 className='text-6xl font-[400]'>Where Money Grows</h1>
       <h4 className='w-120 text-center text-xl font-[500] text-gray-600'>A programmable, utility-driven stable token designed for native value accrual and seamless integration into Defi.</h4>
       <button className='bg-black rounded-4xl text-white px-8 py-1'>Try it now</button>
--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -4,7 +4,7 @@ const Partners = () => {
   return (
     <div className='flex justify-between items-center gap-10 mt-15 mb-30'>
       <h5 className='w-60 text-gray-600 font-[500]'>Backed by the best companies and visionary angels.</h5>
-        <img className='w-[60%] aspect-auto' src="/partners.webp" alt="logo" />
+        <img className='w-[60%] aspect-auto' src="./partners.webp" alt="logo" />
     </div>
   )
 }
