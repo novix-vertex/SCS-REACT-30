ed845a2edb8430c3be24f4b390936f6025981112
chirag jain <jchirag0610@gmail.com>
2026-05-05 15:21:50 +0530

    responsive: explore section

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-
 const Explore = () => {
   return (
-    <div className='flex justify-between items-start pt-10 pb-10'>
+    <div className='md:flex justify-between items-start pt-10 pb-10'>
       <div>
-        <h2 className='text-4xl font-[400]'>What is USD Bloom?</h2>
-        <button className='bg-black rounded-4xl text-white px-8 py-1 mt-2'>Explore now</button>
+        <h2 className='text-center md:text-left text-4xl font-normal'>What is USD Bloom?</h2>
+        <button className='block mx-auto md:mx-0 bg-black rounded-full text-white px-8 py-1 mt-2'>Explore now</button>
       </div>
-      <h4 className='w-120 text-xl font-[500] text-gray-600'>A programmable, utility-driven stable token designed for native value accrual and seamless integration into Defi.</h4>
+      <h4 className='mt-10 md:mt-0 w-full text-center md:text-left md:w-120 text-xl font-medium text-gray-600'>A programmable, utility-driven stable token designed for native value accrual and seamless integration into Defi.</h4>
 
     </div>
   )
