cb6a286ec21db67ae72eb7ae54dbe0260fed75e5
chirag jain <jchirag0610@gmail.com>
2026-05-05 14:43:55 +0530

    responsive: partners section

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 const Partners = () => {
   return (
-    <div className='flex justify-between items-center gap-10 mt-15 mb-30'>
-      <h5 className='w-60 text-gray-600 font-[500]'>Backed by the best companies and visionary angels.</h5>
-        <img className='w-[60%] aspect-auto' src="./partners.webp" alt="logo" />
+    <div className='flex flex-wrap justify-center md:justify-between items-center gap-10 mt-15 mb-15 md:mb-30'>
+      <h5 className='text-center md:text-left md:w-60 text-gray-600 font-semibold'>Backed by the best companies and visionary angels.</h5>
+        <img className='w-full md:w-[60%] aspect-auto' src="./partners.webp" alt="logo" />
     </div>
   )
 }
