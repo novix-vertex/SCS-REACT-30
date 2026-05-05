04905b34412f90ffed352b575cfbb6fc0520daa1
chirag jain <jchirag0610@gmail.com>
2026-05-05 14:39:44 +0530

    responsive: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-
 const Navbar = () => {
     return (
-        <nav className='flex justify-between'>
-            <h4 className="logo font-[600] flex justify-center items-center"> <img src="./logo.webp" alt="" className='h-6' />BloomFi</h4>
-            <menu className='flex gap-15 font-[600]'>
+        <nav className='flex justify-between items-center py-4 px-8'>
+            <h4 className="logo font-semibold flex items-center gap-2"> <img src="./logo.webp" alt="" className='h-6' />BloomFi</h4>
+            <menu className='hidden md:flex gap-8 font-semibold'>
                 <a href="#">USD bloom</a>
                 <a href="#">Business</a>
                 <a href="#">Treasury</a>
                 <a href="#">Developers</a>
                 <a href="#">Join us</a>
             </menu>
-            <button className="launch-beta bg-[#2B273E] text-white rounded-4xl py-2 px-5">Launch BETA</button>
+            <button className="md:hidden text-2xl">
+                ☰
+            </button>
+            <button className="hidden md:inline-block launch-beta bg-[#2B273E] text-white rounded-full py-2 px-4 text-sm md:text-base">Launch BETA</button>
         </nav>
     )
 }
